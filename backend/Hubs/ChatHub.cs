59039c8a361e8c6734265596b8c0f78d0b46a269
Edson Caliman <edsoncaliman@gmail.com>
2022-08-07 18:12:05 -0300

    add backend project with signalr

---
+++ backend/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FinancialChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+
+        public ChatHub()
+        {
+            _botUser = "Chat Bot";
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("receiveMessage", _botUser,
+                $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
